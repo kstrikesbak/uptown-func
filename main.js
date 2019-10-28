fe61eeaa594b98c1883692f9e6b97c52f0537d8e
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-28 19:40:06 -0400

    complete

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
-function call() {
+function call(func) {
+  func();
 }
 
-function callTwice() {
+function callTwice(func) {
+  func();
+  func();
 }
 
-function callXTimes() {
+function callXTimes(func,n) {
+  for (let i=0;i<n;i++) {
+    func()
+  }
 }
 
-function returnFromFunc() {
+function returnFromFunc(func) {
+  return func()
 }
 
-function modifyString() {
+function modifyString(str,func) {
+  return func(str)
 }
 
-function modifyNumber() {
+function modifyNumber(num,func) {
+  return func(num)
 }
 
-function modifyAnything() {
+function modifyAnything(something,func) {
+  return func(something)
+
 }
 
-function twoFuncs() {
+function twoFuncs(func1,func2) {
+  return func2(func1())
 }
 
-function twoValues() {
+function twoValues(val1,val2,func) {
+  return func(val1,val2)
 }
 
-function twoValuesRTL() {
+function twoValuesRTL(val1,val2,func) {
+  return func(val2,val1)
 }
 
 
